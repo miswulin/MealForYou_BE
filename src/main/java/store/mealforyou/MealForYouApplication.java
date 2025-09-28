7b0d953dc8173fc458349683c246c61338066bf9
mxyffff <rua0254@naver.com>
2025-09-29 08:54:31 +0900

    프로젝트 생성
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/store/mealforyou/MealForYouApplication.java
@@ -0,0 +1,11 @@
+package store.mealforyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealForYouApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MealForYouApplication.class, args);
+    }
+}
