c801dee07838a41d1f0658668f0788fe101f3571
yb2527 <yb2527@swu.ac.kr>
2025-10-09 13:42:27 +0900

    feat: Entity, Enum 작성

---
+++ src/main/java/store/mealforyou/entity/Dish.java
@@ -0,0 +1,35 @@
+package store.mealforyou.entity;
+
+import lombok.*;
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Dish {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name; // 요리명
+    private String productInfo; // 상품정보
+    private String sort; // 분류 기준
+
+    private Long mainDishImageId; // 대표 이미지 1장의 Id
+    private Integer basePrice; // 기본구성 가격
+
+    @Lob
+    private String recipe; // 레시피
+
+    // 테이블 매핑
+    @OneToMany(mappedBy = "dish", cascade = CascadeType.ALL)
+    private List<DishIngredient> dishIngredients = new ArrayList<>();
+
+    @OneToMany(mappedBy = "dish", cascade = CascadeType.ALL)
+    private List<DishImage> dishImages = new ArrayList<>();
+
+    @OneToMany(mappedBy = "dish", cascade = CascadeType.ALL)
+    private List<Interest> interests = new ArrayList<>();
+}
